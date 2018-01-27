7667fb70f398a9d940961adabcaf6ca35ff6789f
luongluong <luong2443@gmail.com>
2018-01-27 14:43:17 -0600

    2/ Create vector onfiltered and obunfiltered

--- app/src/main/java/edu/lonestar/framer/DownloadDaemon.java
+++ app/src/main/java/edu/lonestar/framer/DownloadDaemon.java
@@ -1,8 +1,45 @@
 package edu.lonestar.framer;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.lang.String;
+import java.util.Vector;
+import edu.lonestar.framer.util.RemoteImage;
 /**
  * Created by LUONG LUONG on 1/27/2018.
  */
 
-public class DownloadDaemon {
+
+public class DownloadDaemon
+{
+    Vector<RemoteImage> parseString()
+    {
+        // Get http response, include try catch for handle exception
+        Vector<RemoteImage> ob= new Vector<>(); // Work on this one later
+        try
+        {
+            String initialData =  IOUtils.toString(URI.create("http://eventhorizonwebdesign.com/framed/api/index.php/images"));
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return new Vector(); // double check what to return
+    }
+
+    // Vector String to store json lists and for loop to store in unfiltered vector
+        Vector<RemoteImage> obunfiltered= new Vector<>();
+        Vector<String> strings= new Vector<>();// Store the Json Lists
+            strings
+
+
+    // Vector object Filtered
+        Vector<RemoteImage> obfiltered= new Vector<>();
+
+
 }
--- app/src/main/java/edu/lonestar/framer/util/RemoteImage.java
+++ app/src/main/java/edu/lonestar/framer/util/RemoteImage.java
@@ -6,7 +6,27 @@ package edu.lonestar.framer.util;
 
 public class RemoteImage
 {
+    private int id;
+    private int year;
+    private int height;
+    private int width;
+    private String artist;
+    private String url;
+    private String title;
 
+    RemoteImage(int id, int yr, int h,int w,String author,String u, String name)
+    {
+        id=id;
+        year= yr;
+        height= h;
+        width= w;
+        artist= author;
+        url= u;
+        title= name;
+    }
 
+    RemoteImage(String json)    // Split Json string
+    {
 
+    }
 }
