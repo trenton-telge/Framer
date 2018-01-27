ad8a643af648eb33cf98e7d023589cffe80e9ccf
Trenton Telge <darkforce100@users.noreply.github.com>
2018-01-27 16:51:50 -0600

    URLConnection for API request

--- app/src/main/java/edu/lonestar/framer/DownloadDaemon.java
+++ app/src/main/java/edu/lonestar/framer/DownloadDaemon.java
@@ -2,9 +2,11 @@ package edu.lonestar.framer;
 
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
 import java.lang.String;
+import java.net.URLConnection;
 import java.util.Vector;
 import edu.lonestar.framer.util.RemoteImage;
 /**
@@ -21,12 +23,23 @@ public class DownloadDaemon
         Vector<RemoteImage> ob= new Vector<>(); // Work on this one later
         try
         {
-            String initialData = ""; //TODO
-            new RemoteImage(initialData);
+            URL url = new URL("http://eventhorizonwebdesign.com/framed/api/index.php/images");
+            URLConnection urlConnection = url.openConnection();
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+
+            int numCharsRead;
+            char[] charArray = new char[1024];
+            StringBuffer sb = new StringBuffer();
+            while ((numCharsRead = isr.read(charArray)) > 0) {
+                sb.append(charArray, 0, numCharsRead);
+            }
+            String result = sb.toString();
+            new RemoteImage(result);
             //ector String to store json lists and
             Vector<String> strings= new Vector<>();// Store the Json Lists
-            strings.add(initialData.substring(0, initialData.indexOf("},{") + 1)); // copy to vector
-            initialData = initialData.substring(initialData.indexOf("},{") + 1, initialData.length());
+            strings.add(result.substring(0, result.indexOf("},{") + 1)); // copy to vector
+            result = result.substring(result.indexOf("},{") + 1, result.length());
             // Remove the one which is just copied
 
             //  for loop to store in unfiltered vector
