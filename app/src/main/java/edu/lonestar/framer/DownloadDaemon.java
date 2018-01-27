e14adab1ca7b1999a2063833ac9b3ef80061d470
luongluong <luong2443@gmail.com>
2018-01-27 13:08:00 -0600

    Create RemoteImage and download Daemon

---
+++ app/src/main/java/edu/lonestar/framer/DownloadDaemon.java
@@ -0,0 +1,8 @@
+package edu.lonestar.framer;
+
+/**
+ * Created by LUONG LUONG on 1/27/2018.
+ */
+
+public class DownloadDaemon {
+}
---
+++ app/src/main/java/edu/lonestar/framer/util/RemoteImage.java
@@ -0,0 +1,12 @@
+package edu.lonestar.framer.util;
+
+/**
+ * Created by LUONG LUONG on 1/27/2018.
+ */
+
+public class RemoteImage
+{
+
+
+
+}
